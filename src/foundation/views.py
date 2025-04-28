42255a93730a081a0df40bbef5474c616d2f4dc5
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2025-04-28 17:19:43 +0530

    error solved

--- src/foundation/views.py
+++ src/foundation/views.py
@@ -8,16 +8,16 @@ def homePageView(request):
         "Page_Title": "Home Page",
         "Page_Visits": visits.count()
     }
-    PageVisit.objects.create(path=request.path)
+    PageVisit.objects.create(page=request.path)
     # return HttpResponse('<h1>Hello</h1>')
     return render(request, 'home.html', my_context)
 
 def aboutPageView(request):
-    visits = PageVisit.objects.filter(path=request.path) 
+    visits = PageVisit.objects.filter(page=request.path) 
     my_context = {
         "Page_Title": "About Page",
         "Page_Visits": visits.count()
     }
-    PageVisit.objects.create(path=request.path)
+    PageVisit.objects.create(page=request.path)
     # return HttpResponse('<h1>Hello</h1>')
     return render(request, 'home.html', my_context)
